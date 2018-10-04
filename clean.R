b1796ecbb599c1c00309fab4add29d279ce8bc14
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 13:33:04 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,19 @@
 
+
 clean_data <- raw_data
+#re-use the code from before
+readStates<-function(states)
+{
+    states<- states[-1,]
+    num.row<- nrow(states)
+    states<- states[-num.row,]
+    
+    states<- states[,-1:-4]
+    
+    colnames(states)<- c("stateName","population","popOver18","percentOver18")
+    
+    return(states)
+}
+
+cleanCensus<- readStates(raw_data)
+str(cleanCensus)
